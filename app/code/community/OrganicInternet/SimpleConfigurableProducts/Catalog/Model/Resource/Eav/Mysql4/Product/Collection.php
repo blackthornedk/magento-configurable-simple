a0598bd261ba2f016136acd21265f83eebaca20e
Jacob V. Rasmussen <jvr@glokal-marketing.dk>
2015-11-11 15:53:47 +0100

    Updated Product Collection to 1.9.x
    The Product collection was among other things missing 'tax_class_id', which prevented
    Magento from showing prices incl. tax on product collections.

--- app/code/community/OrganicInternet/SimpleConfigurableProducts/Catalog/Model/Resource/Eav/Mysql4/Product/Collection.php
+++ app/code/community/OrganicInternet/SimpleConfigurableProducts/Catalog/Model/Resource/Eav/Mysql4/Product/Collection.php
@@ -11,29 +11,35 @@ class OrganicInternet_SimpleConfigurableProducts_Catalog_Model_Resource_Eav_Mysq
             return $this;
         }
 
+        $helper     = Mage::getResourceHelper('core');
         $connection = $this->getConnection();
-
+        $select     = $this->getSelect();
         $joinCond = $joinCond = join(' AND ', array(
             'price_index.entity_id = e.entity_id',
             $connection->quoteInto('price_index.website_id = ?', $filters['website_id']),
             $connection->quoteInto('price_index.customer_group_id = ?', $filters['customer_group_id'])
         ));
 
-        $fromPart = $this->getSelect()->getPart(Zend_Db_Select::FROM);
+        $fromPart = $select->getPart(Zend_Db_Select::FROM);
         if (!isset($fromPart['price_index'])) {
-            $minimalExpr = new Zend_Db_Expr(
-                'IF(`price_index`.`tier_price`, LEAST(`price_index`.`min_price`, `price_index`.`tier_price`), `price_index`.`min_price`)'
-            );
-            $indexedExpr = new Zend_Db_Expr('price_index.price');
-            $this->getSelect()->join(
-                array('price_index' => $this->getTable('catalog/product_index_price')),
-                $joinCond,
-                array('indexed_price'=>$indexedExpr,'price', 'final_price', 'minimal_price'=>$minimalExpr , 'min_price', 'max_price', 'tier_price'));
-
+            $least       = $connection->getLeastSql(array('price_index.min_price', 'price_index.tier_price'));
+            $minimalExpr = $connection->getCheckSql('price_index.tier_price IS NOT NULL',
+                $least, 'price_index.min_price');
+	    $indexedExpr = new Zend_Db_Expr('price_index.price');
+	    $colls       = array('indexed_price' => $indexedExpr, 'price', 'tax_class_id', 'final_price',
+                'minimal_price' => $minimalExpr, 'min_price', 'max_price', 'tier_price');
+            $tableName = array('price_index' => $this->getTable('catalog/product_index_price'));
+	    $select->join($tableName, $joinCond, $colls);
+            // Set additional field filters
+            foreach ($this->_priceDataFieldFilters as $filterData) {
+                $select->where(call_user_func_array('sprintf', $filterData));
+            }
         } else {
             $fromPart['price_index']['joinCondition'] = $joinCond;
-            $this->getSelect()->setPart(Zend_Db_Select::FROM, $fromPart);
+            $select->setPart(Zend_Db_Select::FROM, $fromPart);
         }
+        //Clean duplicated fields
+        $helper->prepareColumnsList($select);
 
         return $this;
     }
